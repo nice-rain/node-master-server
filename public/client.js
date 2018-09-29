df66e1d717bde54ed3c9520a2d6310c30af918d8
Walker Twyman <walk12288@gmail.com>
2018-09-29 14:57:49 -0500

    Added CORS to server. Updated client.js to refresh server list.

---
+++ public/client.js
@@ -0,0 +1,55 @@
+'use strict'
+
+//Function that will loop through the entire response and build a row for each index.
+//The row will contain the servername and IP address (we can always add more information later).
+function buildTableRows(res)
+{
+    //string that will hold all built rows
+    let rows;
+    
+
+
+    //For loop for each server in our response
+    for(let i = 0; i < res.length; i++)
+    {
+        //Add each row to our stored rows
+        rows += `<td>${res[i].serverName}</td> 
+        <td>${res[i].serverIP}:${res[i].serverPort}</td>`;
+    }
+
+    updateTable(rows);
+}
+
+
+//Function updates our table with headings and generated rows
+function updateTable(rows)
+{
+    let headings = `<tr><th>Server Name</th><th>IP Address</th></tr>`
+
+    $('.js-server-list').html(headings + rows);
+}
+
+//Issues a GET request to return all servers. On success, will call buildTableRows to add rows to html.
+function RefreshTable()
+{
+    //Get request to our server
+    $.getJSON("http://localhost:8082/server", (res) =>{
+        
+        //Check to make sure we have at least 1 server if we are going to update table
+        if(res.length > 0)
+        {
+            buildTableRows(res);
+        }
+        else
+        {
+            console.log("Warning: Our response had no listed servers.");
+        }
+    })
+    .catch(err=>{
+        console.log(err);
+    });
+}
+
+
+//Automatically Refresh our List
+$(RefreshTable());
--- server.js
+++ server.js
@@ -34,6 +34,16 @@ app.use(express.json());
 
 
 //=======================================================
+// CORS
+//=======================================================
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+//=======================================================
 // GET Endpoint - Return Server List
 //=======================================================
 
