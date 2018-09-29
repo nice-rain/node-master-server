054c0d3328247bef270f28d047dc4a4156c11922
Walker Twyman <walk12288@gmail.com>
2018-09-29 16:29:04 -0500

    Added heartbeat and PUT endpoints

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ const app = express();
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
+//Mongoose fix deprecation warning
+mongoose.set('useFindAndModify', false);
+
 //Import the database url and port we'll use for this application
 const {DATABASE_URL, PORT} = require("./config");
 
@@ -71,6 +74,48 @@ app.get('/server', (req,res) => {
 
 //GET /:id/:port will allow us to refresh the server heartbeat. We will return a success status once we refresh.
 //On a failed status, the game will know that it needs to send a POST to register with the master server.
+app.get('/server/:id/:port', (req,res) =>
+{
+  Servers.findById(req.params.id)
+  .then(server =>{
+    
+    //Validate our port matches
+    let port = parseInt(req.params.port);
+    if(server.serverPort !== port)
+    {
+      const message = `Request Port: ${req.params.port} does not match server port.`;
+      console.log(message);
+      res.status(400).send(message);
+    }
+
+    //Validate that our server IP matches
+    else if(server.serverIP !== req.ip)
+    {
+      const message = `Request IP: ${req.ip} does not match stored IP.`;
+      console.log(message);
+      res.status(400).send(message);
+    }
+
+    //Both port and IP match
+    else{
+      //Find our server in the database
+      Servers.findOneAndUpdate({_id: req.params.id}, {$set: {updated: Date.now()}})
+      .then(updatedServer =>{
+        console.log(updatedServer.serverName);
+        res.status(200).send('Heartbeat Successful');
+      })
+      .catch(err =>{
+        console.log(`\n\nError: ${err}`);
+        res.status(500).json({error: "internal server error"});
+      })
+    }
+  })
+
+  //Error finding ID
+  .catch(err =>{
+    res.status(500).json({error: "Server may not exist. POST to register server."});
+  });
+});
 
 //=======================================================
 // POST Endpoint - Register a new server
@@ -115,10 +160,72 @@ app.post('/server', (req, res)=>
 });
 
 
+//=======================================================
+// PUT Endpoint
+//=======================================================
 
+//PUT /:id/:port endpoint - Allows us to change server name, or number of players
+app.put('/server/:id/:port', (req, res) =>
+{
+  //validate our IP address and Port 
+  Servers.findById(req.params.id)
+  .then(server =>{
+    
+    //Validate our port matches
+    let port = parseInt(req.params.port);
+    if(server.serverPort !== port)
+    {
+      const message = `Request Port: ${req.params.port} does not match server port.`;
+      console.log(message);
+      res.status(400).send(message);
+    }
 
+    //Validate that our server IP matches
+    else if(server.serverIP !== req.ip)
+    {
+      const message = `Request IP: ${req.ip} does not match stored IP.`;
+      console.log(message);
+      res.status(400).send(message);
+    }
 
-//PUT /:id/:port endpoint - Allows us to change server name, port, or number of players
+    //Validate our ID in our body matches the ID in the parameter
+    else if(!(req.params.id && req.body.id && req.params.id === req.body.id)) 
+    {
+      const message = `Request Path ID: ${req.ip} and request body ID must match.`;
+      console.log(message);
+      res.status(400).send(message);
+    }
+
+    //Both port and IP match
+    else{
+      
+      //We need to check to see which fields we are passing to update
+
+      const updated = {};
+      //We can update this to allow fields such as playerCount
+      const updateableFields = ['serverName'];
+      updateableFields.forEach(field => {
+        if (field in req.body) {
+          updated[field] = req.body[field];
+        }
+      });
+      
+      //Update our server's fields
+      Servers.findByIdAndUpdate(req.params.id, { $set: updated }, { new: true })
+        .then(updatedServer => {
+          console.log("PUT request successful, server fields updated.")
+          res.status(204).end()
+        })
+        .catch(err => res.status(500).json({ message: 'Internal Sever Error' }));
+    }
+  })
+
+  //Error finding ID
+  .catch(err =>{
+    res.status(500).json({error: "Server may not exist. POST to register server."});
+  });
+
+});
 
 //DELETE /:id/:port endpoint - Called when we shutdown server normally. Will automatically remove it from the master server database.
 
