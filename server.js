13dc5ce9d80f4d18ca4aeba13f1970ff882bf3e1
Walker Twyman <walk12288@gmail.com>
2018-09-28 22:36:42 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+'use strict'
+
+//Imports
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+
+app.listen(process.env.PORT, () => {
+    console.log(`Your app is listening on port ${process.env.PORT}`);
+  });
+  
