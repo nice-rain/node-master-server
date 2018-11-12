901f2a42be669b051622680fb523996a8e107863
Walker Twyman <walk12288@gmail.com>
2018-11-12 17:36:35 -0600

    Updated faker to use a random word.

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 'use strict'
 
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/master-server';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://master:8VdMr7gjHCumCAM@ds033915.mlab.com:33915/master-server';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-master-server';
 module.exports.PORT = process.env.PORT || 8082; //server port we are listening on
